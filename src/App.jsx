77e3ecb5aaa0e95d90e463cfe0f2d9d4cf0ddf12
Alona Samoilenko <maks.alenka@gmail.com>
2024-11-05 12:18:32 -0500

    Created lis

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,19 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
+const todoList = [
+  {id: 1, title: 'Read a book'},
+  {id: 2, title: 'Do homework'},
+  {id: 3, title: 'Complete assignment'},
+]
 function App() {
   const [count, setCount] = useState(0);
 
   return (
     <>
-    <h1> Todo list </h1>
+    <ul>
+      { todoList.map((item) => (<li key={item.id}>{item.title}</li>)) }
+    </ul>
     </>
   );
 }
