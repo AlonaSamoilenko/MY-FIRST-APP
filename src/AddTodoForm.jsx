c2be91b9e7cb2114ce262113e5aa494dc9406f62
Alona Samoilenko <maks.alenka@gmail.com>
2024-11-19 22:53:27 -0500

    submited AddTodoForm

--- src/AddTodoForm.jsx
+++ src/AddTodoForm.jsx
@@ -1,9 +1,18 @@
 function AddTodoForm() {
+    function handleAddTodo(event){
+       event.preventDefault();
+
+       const todoTitle = event.target.title.value;
+       console.log(todoTitle);
+    }
     return (
         <>
-            <form>
+            <form onSubmit = {handleAddTodo}>
                 <label htmlFor="todoTitle"> Title:
-                    <input id="todoTitle" />
+                    <input id="todoTitle" 
+                    type = "text"
+                    name = "title"
+                    />
                 </label>
             </form>
             <button type="submit"> Add </button>
