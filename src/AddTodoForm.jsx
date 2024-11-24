1f0966a91368345bf9efe7b1d9f96671561692f5
Alona Samoilenko <maks.alenka@gmail.com>
2024-11-24 17:11:42 -0500

    fixed error:moved submit button into form

--- src/AddTodoForm.jsx
+++ src/AddTodoForm.jsx
@@ -5,21 +5,26 @@ function AddTodoForm(props) {
       
        const todoTitle = event.target.title.value;
        console.log(todoTitle);
-                                    
+       
        props.onAddTodo(todoTitle);
     }
     return (
         <>
             <form onSubmit = {handleAddTodo}>
-                <label htmlFor="todoTitle"> Title:
-                    <input id="todoTitle" 
+                <label htmlFor="todoTitle"> 
+                    Title:
+                    <input 
+                    id="todoTitle" 
                     type = "text"
                     name = "title"
+                    placeholder="New Todo:"
                     />
                 </label>
+                <hr/>
+                <button type="submit"> Add </button>
             </form>
-            <button type="submit"> Add </button>
+            
         </>
     )
 }
-export default AddTodoForm
+export default AddTodoForm;
--- src/App.jsx
+++ src/App.jsx
@@ -4,14 +4,16 @@ import viteLogo from '/vite.svg'
 import './App.css'
 import TodoList from './TodoList.jsx'
 import AddTodoForm from './AddTodoForm.jsx'
+
 function App() {
   const [count, setCount] = useState(0);
   const [newTodo, setNewTodo] = useState('');
+
   return (
     <>
     <h1> Todo List </h1>
     <AddTodoForm onAddTodo={setNewTodo} />
-    <p> New Todo: {newTodo} </p>
+    <p>  {newTodo} </p>
     <TodoList/>
     </>
   );
